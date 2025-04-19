1ee8474fa93f95495e996caa3e8a8862eee49950
David Rhoderick <david.e.rhoderick@gmail.com>
2025-04-19 16:46:38 +0200

    Adding queries for lists and items

--- main.py
+++ main.py
@@ -5,10 +5,29 @@ from strawberry.fastapi import GraphQLRouter
 
 
 @strawberry.type
+class List:
+    id: strawberry.ID
+    name: str
+
+
+@strawberry.type
+class Item:
+    id: strawberry.ID
+    name: str
+    description: str = None
+    completed: bool = False
+    children: list["Item"] = None
+
+
+@strawberry.type
 class Query:
     @strawberry.field
-    def hello(self) -> str:
-        return "Hello World"
+    def lists(self) -> list[List]:
+        return []
+
+    @strawberry.field
+    def list(self, id: strawberry.ID) -> list[Item]:
+        return []
 
 
 schema = strawberry.Schema(Query)
