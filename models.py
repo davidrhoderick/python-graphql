7b65b1d3a9503b384b37f9820aab6ca7049a3782
David Rhoderick <david.e.rhoderick@gmail.com>
2025-04-19 21:05:11 +0200

    Adding models

---
+++ models.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class ListModel(Base):
+    __tablename__ = "lists"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+    items = relationship("ItemModel", back_populates="list")
+
+
+class ItemModel(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    completed = Column(Boolean, default=False)
+    list_id = Column(Integer, ForeignKey("lists.id"))
+
+    list = relationship("ListModel", back_populates="items")
+    children = relationship("SubItemModel", back_populates="parent_item")
+
+
+class SubItemModel(Base):
+    __tablename__ = "subitems"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    completed = Column(Boolean, default=False)
+    parent_item_id = Column(Integer, ForeignKey("items.id"))
+
+    parent_item = relationship("ItemModel", back_populates="children")
