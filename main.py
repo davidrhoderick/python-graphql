35d4b58d3f565ccf404a1355c37e84507072a086
David Rhoderick <david.e.rhoderick@gmail.com>
2025-04-19 17:00:09 +0200

    Adding mutations

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 
 
+@strawberry.input
+class ListInput:
+    id: strawberry.ID = None
+    name: str
+
+
+@strawberry.input
+class ItemInput:
+    listId: strawberry.ID
+    id: strawberry.ID = None
+    name: str
+    description: str = None
+
+
 @strawberry.type
 class List:
     id: strawberry.ID
@@ -30,7 +44,31 @@ class Query:
         return []
 
 
-schema = strawberry.Schema(Query)
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def put_list(self, input: ListInput) -> List:
+        if not input.id:
+            id = "123456"
+        else:
+            id = input.id
+        return List(id=id, name=input.name)
+
+    @strawberry.mutation
+    def put_item(self, input: ItemInput) -> Item:
+        if not input.id:
+            id = "123456"
+        else:
+            id = input.id
+        return Item(
+            id=id,
+            name=input.name,
+            description=input.description,
+            completed=False,
+        )
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 
 graphql_app = GraphQLRouter(schema)
 
